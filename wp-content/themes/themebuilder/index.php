1684d6cda45d9350ee82dcb6ce5d348423a330f1
Chris <166389@student.horizoncollege.nl>
2023-06-21 11:26:16 +0200

    footer toegevoegd

--- wp-content/themes/themebuilder/index.php
+++ wp-content/themes/themebuilder/index.php
@@ -49,8 +49,22 @@
     </main>
 
     <footer>
-        <!-- Voeg hier je footerinhoud toe -->
+        <div class="container">
+            <div class="footer-content">
+                <p>© <?php echo date('Y'); ?> MijnBedrijf. Alle rechten voorbehouden.</p>
+                <p>Thema gemaakt door <a href="https://github.com/166389" target="_blank">Chris Groen</a></p>
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer-menu',
+                    'container' => false,
+                    'menu_class' => 'footer-menu',
+                    'fallback_cb' => false
+                ));
+                ?>
+            </div>
+        </div>
     </footer>
+
 </body>
 
 </html>
